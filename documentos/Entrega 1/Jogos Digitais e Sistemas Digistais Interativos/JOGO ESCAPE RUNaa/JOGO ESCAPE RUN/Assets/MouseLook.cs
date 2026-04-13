33a391c956ca13321449a6626d35db00bf1bd235
Guilherme Alves <guilhermealvespaulo@outlook.com>
2026-04-13 19:19:24 -0300

    Jogo_Atualizado

---
+++ documentos/Entrega 1/Jogos Digitais e Sistemas Digistais Interativos/JOGO ESCAPE RUNaa/JOGO ESCAPE RUN/Assets/MouseLook.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 200f;
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    public float minLookAngle = -5f;
+    public float maxLookAngle = 20f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, minLookAngle, maxLookAngle);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        
+        if (playerBody != null)
+        {
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+    }
+}
